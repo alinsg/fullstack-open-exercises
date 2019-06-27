4bc922ff833e3f2d638d1de17f984cc9884287bd
Alin Simion <simion.g.alin@gmail.com>
2019-06-27 14:35:27 +0300

    Course component is now separated in own module

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Header = props => <h1>{props.name}</h1>
+
+const Content = props =>
+  props.parts.map(part => <Part part={part} key={part.name} />)
+
+const Total = props => {
+  const total = props.parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+  return <p style={{ fontWeight: 'bold' }}>total of {total} exercises</p>
+}
+
+const Part = props => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Course = props => {
+  const { course } = props
+  const { name, parts } = course
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default Course
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,35 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = props => <h1>{props.name}</h1>
-
-const Content = props =>
-  props.parts.map(part => <Part part={part} key={part.name} />)
-
-const Total = props => {
-  const total = props.parts.reduce((sum, part) => {
-    return sum + part.exercises
-  }, 0)
-  return <p style={{ fontWeight: 'bold' }}>total of {total} exercises</p>
-}
-
-const Part = props => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Course = props => {
-  const { course } = props
-  const { name, parts } = course
-  return (
-    <div>
-      <Header name={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const course = {
