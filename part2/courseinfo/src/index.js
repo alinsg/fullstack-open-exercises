4bf36076d2d30e6e78b571e4f932b3d4aa1ddcd7
Alin Simion <simion.g.alin@gmail.com>
2019-06-27 14:25:26 +0300

    Refactored App component so it uses a Course component for each course

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = props => <h1>{props.course}</h1>
+const Header = props => <h1>{props.name}</h1>
 
 const Content = props =>
   props.parts.map(part => <Part part={part} key={part.name} />)
@@ -17,28 +17,39 @@ const Part = props => (
   </p>
 )
 
+const Course = props => {
+  const { course } = props
+  const { name, parts } = course
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Course course={course} />
     </div>
   )
 }
