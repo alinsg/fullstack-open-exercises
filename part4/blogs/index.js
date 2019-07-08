89c2a89113ee79a39ba263b36543399be5ce56c7
Alin Simion <simion.g.alin@gmail.com>
2019-07-08 15:01:53 +0300

    Index.js now requires the app module

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -1,27 +1,8 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Blog = require('./models/blog')
+const app = require('./app')
 const config = require('./utils/config')
 
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
+const server = http.createServer(app)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
