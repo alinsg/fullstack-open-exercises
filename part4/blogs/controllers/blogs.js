ff7afe8b8ea3e5a8cedd75f40ce7802d04bc4e83
Alin Simion <simion.g.alin@gmail.com>
2019-07-12 17:23:01 +0300

    Refactored controller to use async await

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,49 +1,50 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
     response.json(blogs.map(blog => blog.toJSON()))
-  })
+  } catch (e) {
+    next(e)
+  }
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog.toJSON())
-      } else {
-        response.status(404).end()
-      }
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    blog ? response.json(blog.toJSON()) : response.status(404).end()
+  } catch (e) {
+    next(e)
+  }
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const body = request.body
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0
     })
-    .catch(error => next(error))
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
+  } catch (e) {
+    next(e)
+  }
 })
 
-blogsRouter.post('/', (request, response, next) => {
-  const body = request.body
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0
-  })
-  blog
-    .save()
-    .then(savedBlog => {
-      response.json(savedBlog.toJSON())
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (e) {
+    next(e)
+  }
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
   const blog = {
     title: body.title,
@@ -51,11 +52,14 @@ blogsRouter.put('/:id', (request, response, next) => {
     url: body.url,
     likes: body.likes
   }
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog.toJSON())
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+      new: true
     })
-    .catch(error => next(error))
+    response.json(updatedBlog.toJSON())
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = blogsRouter
