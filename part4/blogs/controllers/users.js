4d0858ff49110cbf37cd76309c39fa2e477ec46e
Alin Simion <simion.g.alin@gmail.com>
2019-07-13 12:47:38 +0300

    Added tests for user creation

--- part4/blogs/controllers/users.js
+++ part4/blogs/controllers/users.js
@@ -13,7 +13,13 @@ usersRouter.post('/', async (request, response, next) => {
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
-
+    //Password validation
+    if (body.password.length < 3) {
+      return response
+        .status(400)
+        .json({ error: 'password must be at least 3 characters long' })
+    }
+    //
     const user = new User({
       username: body.username,
       name: body.name,
---
+++ part4/blogs/tests/user_api.test.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const User = require('../models/user')
+
+const initialUsers = [
+  {
+    username: 'root',
+    name: 'John Doe',
+    password: 'foobar'
+  }
+]
+
+describe('when there is only one user in db', () => {
+  //Before each test, db users data will be deleted
+  beforeEach(async () => {
+    await User.deleteMany({})
+    let userObject = new User(initialUsers[0])
+    await userObject.save()
+  })
+
+  test('user creation is successful', async () => {
+    const newUser = {
+      username: 'ijones',
+      name: 'Indiana Jones',
+      password: 'test1234'
+    }
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/users')
+    expect(response.body.length).toBe(initialUsers.length + 1)
+  })
+
+  test('user creation with an invalid username should not be successful', async () => {
+    const newUser = {
+      username: 'ij',
+      name: 'Indiana Jones',
+      password: 'test1234'
+    }
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+  })
+
+  test('user creation with a property missing should not be successful', async () => {
+    const newUser = {
+      name: 'Indiana Jones',
+      password: 'test1234'
+    }
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+  })
+
+  test('user creation when password is smaller than 3 chars should not be successful', async () => {
+    const newUser = {
+      username: 'ijones',
+      name: 'Indiana Jones',
+      password: '12'
+    }
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+afterAll(async () => {
+  mongoose.connection.close()
+})
